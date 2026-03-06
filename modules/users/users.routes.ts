8644e3c5515038133c74e970fd987d470ab6d4ce
ricardo polanco <luchen668@gmail.com>
2026-03-05 21:21:27 -0400

    Actualizao

--- modules/users/users.routes.ts
+++ modules/users/users.routes.ts
@@ -5,7 +5,7 @@ import { UserMemoryRepository } from "./users.memory-repository";
 
 const router = Router();
 
-// Composición raíz para el módulo de usuarios
+// Composición raíz para el módulo de usuarios-Weo lui , haciendo eto me dolia el caco, btw se me dano la tecla q y d, parece que tienen un falso conTACTO
 const repo = new UserMemoryRepository();
 const svc = new UsersService(repo);
 const controller = new UsersController(svc);
